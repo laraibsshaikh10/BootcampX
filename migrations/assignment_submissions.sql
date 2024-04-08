f813d148ccd998943cf53d6f88c15ddd03516202
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-04-07 23:52:13 -0400

    Created and added data to the teachers and assistance_requests tables

--- migrations/assignment_submissions.sql
+++ migrations/assignment_submissions.sql
@@ -15,22 +15,3 @@ CREATE TABLE assignment_submissions (
   duration INTEGER,
   submission_date DATE
 );
-
-
-CREATE TABLE cohorts (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  start_date DATE,
-  end_date DATE
-);
-
-CREATE TABLE students (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255),
-  phone VARCHAR(32),
-  github VARCHAR(255),
-  start_date DATE,
-  end_date DATE,
-  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
-);
